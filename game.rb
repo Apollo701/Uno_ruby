561c6f2fc9109d0a4f1f004f8eb29c013f2b96d0
Jason Dorn <jdorn@mail.sfsu.edu>
2015-01-20 21:58:28 -0800

    brought back main.rb to possible remove clutter in game class. Game class has initialize method along with greeting

--- game.rb
+++ game.rb
@@ -1,2 +1,26 @@
 class Game
+	attr_accessor :num_of_players, :num_of_rounds, :winner
+	def initialize
+		puts greeting
+		#Round.new(greeting)
+		
+	end
+
+	def greeting
+		puts '' * 4
+		puts "\t"*2 + '*'*80
+		puts "\t"*2 + '*'*80
+		puts "\t"*2 + '*'*21 + ' Welcome to Jason Dorn\'s game of Uno! ' + '*'*21
+		puts "\t"*2 + '*'*80
+		puts "\t"*2 + '*'*80
+		puts '' * 4
+		puts "\t"*2 + 'The game will have 5 people at all times consisting of players and computers.'
+		puts 
+		print "\t"*2 + 'How many people will be playing? '
+		num_of_players = gets.chomp.strip
+	end
+
+
 end
+
+Game.new
